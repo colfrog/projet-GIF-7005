987147baac5a7ef6b864a71602a212ebbb449f15
Laurent Cimon <Laurent.Cimon.1@ulaval.ca>
2025-11-05 12:16:29 -0500

    Classification MLP et SVM

---
+++ mlp.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.optim import SGD
+
+from sklearn.model_selection import train_test_split
+
+# Lire le csv et le convertir en pytorch
+ds = pd.read_csv('donnees_traitees_classification.csv')
+ds = ds.to_numpy()
+ds = torch.from_numpy(ds).float()
+
+# Définir les données et les targets
+X = ds[:, :-1]
+y = ds[:, -1].long()
+
+# Splitter pour le test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# Régler la device
+device = 'cpu'
+
+# Définition du MLP
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.l1 = nn.Linear(X.shape[1], 300) # Couche d'entrée
+        self.l2 = nn.Linear(300, 300) # Couche cachée
+        self.l3 = nn.Linear(300, 300) # Couche cachée
+        self.output = nn.Linear(300, 3) # Couche de sortie
+
+    def forward(self, x):
+        x = F.relu(self.l1(x))
+        x = F.relu(self.l2(x))
+        x = F.relu(self.l3(x))
+        x = F.softmax(self.output(x), dim=-1) # On fait un softmax pour améliorer la classification
+        return x
+
+# Définir le modèle
+net = Net()
+
+# Mettre sur la GPU si possible
+net = net.to(device)
+X_train = X_train.to(device)
+X_test = X_test.to(device)
+y_train = y_train.to(device)
+y_test = y_test.to(device)
+
+# Entraînement du modèle
+optim = SGD(net.parameters(), lr=0.01)
+criterion = nn.CrossEntropyLoss()
+epochs = 500
+loss = None
+for epoch in range(epochs):
+    optim.zero_grad()
+
+    output = net(X_train)
+    loss = criterion(output, y_train)
+    loss.backward()
+
+    optim.step()
+
+    print(epoch, loss.item())
+
+# Sauvegarder le réseau entraîné
+torch.save(net.state_dict(), 'heart-attack-mlp.pt2')
+
+# Calculer l'erreur d'entraînement
+output = net(X_train)
+correct = output.argmax(1).eq(y_train).sum().item()
+erreur_train = 1 - correct/X_train.shape[0]
+
+# Calculer l'erreur en test
+output = net(X_test)
+correct = output.argmax(1).eq(y_test).sum().item()
+erreur_test = 1 - correct/X_test.shape[0]
+
+results = pd.DataFrame({'epochs': [epochs], 'perte finale': [loss.item()], 'erreur entraînement': [erreur_train], 'erreur test': [erreur_test]})
+results.to_csv('mlp.csv', index=False)
---
+++ svm.py
@@ -0,0 +1,50 @@
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+import numpy as np
+import pandas as pd
+
+# Lire le csv et le convertir en pytorch
+ds = pd.read_csv('donnees_traitees_classification.csv')
+ds = ds.to_numpy()
+
+# Définir les données et les targets
+X = ds[:, :-1]
+y = ds[:, -1]
+
+# Splitter pour le test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+
+### Recherche en grille
+valeurs_C = [0.1, 1.0, 10.0, 100.0]
+valeurs_gamma = [1, 5, 10, 15, 20]
+best_C, best_gamma, best_erreur = 0, 0, 1
+
+for C in valeurs_C:
+    for gamma in valeurs_gamma:
+        # Définir le modèle
+        model = SVC(kernel='rbf', C=C, gamma=gamma)
+
+        # Entraîner le modèle
+        model.fit(X_train, y_train)
+
+        # Calculer l'erreur
+        erreur_train = 1 - model.score(X_train, y_train)
+        erreur_test = 1 - model.score(X_test, y_test)
+
+        # On minimise l'erreur sur le test pour éviter d'overfitter
+        if erreur_test < best_erreur:
+            best_C, best_gamma, best_erreur = C, gamma, erreur_test
+
+        print(f'C: {C}, gamma: {gamma}')
+        print(f"Erreur d'entraînement: {erreur_train}, Erreur de test: {erreur_test}")
+
+# Reprendre le meilleur modèle et calculer les erreurs
+model = SVC(kernel='rbf', C=best_C, gamma=best_gamma)
+model.fit(X_train, y_train)
+erreur_train = 1 - model.score(X_train, y_train)
+erreur_test = 1 - model.score(X_test, y_test)
+
+# Sauvegarder les résultats
+resultats = pd.DataFrame({'C': [best_C], 'gamma': [best_gamma], 'erreur entraînement': [erreur_train], 'erreur test': [erreur_test]})
+resultats.to_csv('svm.csv', index=False)
